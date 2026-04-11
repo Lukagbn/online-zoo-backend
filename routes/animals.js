e7c61414ad45a10dff0ad057412303184a5c72b6
lukagbn <luka68690@gmail.com>
2026-04-12 00:41:32 +0400

    fix: add find method to fetch every animal avaliable on animals route

--- routes/animals.js
+++ routes/animals.js
@@ -4,7 +4,7 @@ const Animals = require("../models/Animals.js");
 
 router.get("/", async (req, res) => {
   try {
-    const animals = await Animals();
+    const animals = await Animals.find();
     res.status(200).json({ data: animals });
   } catch (error) {
     res.status(500).json({ error: error.message });
@@ -13,11 +13,6 @@ router.get("/", async (req, res) => {
 router.post("/", async (req, res) => {
   try {
     const { name, commonName, description } = req.body;
-    const existingAnimal = await Animals.findOne({
-      name,
-      commonName,
-      description,
-    });
     if (
       name === undefined ||
       commonName === undefined ||
@@ -26,17 +21,21 @@ router.post("/", async (req, res) => {
       return res.status(400).json({ message: "Every field must be entered!" });
     }
 
+    const existingAnimal = await Animals.findOne({
+      name,
+      commonName,
+      description,
+    });
+
     if (existingAnimal) {
       return res.status(400).json({ error: "Animal already exists!" });
     }
-    const newAnimal = new Animals({
-      name: name,
-      commonName: commonName,
-      description: description,
-    });
+    const newAnimal = new Animals({ name, commonName, description });
     await newAnimal.save();
-    res.status(200).json({ message: "Animal description created!" });
-  } catch (error) {}
+    res.status(201).json({ message: "Animal description created!" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 module.exports = router;
